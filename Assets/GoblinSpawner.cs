5bbfcbbd4bcd38995c3a702ba1ee1ff915a0dc00
Antonio Polanco <antonio.polanco.lopez2@gmail.com>
2023-02-04 19:27:07 +0100

    admlakwd

---
+++ Assets/GoblinSpawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private MoveDirection goblinMoveDirection;
+
+    [SerializeField]
+    private Goblin goblinPrefab;
+
+    [SerializeField]
+    private int spawnDelayAtStart;
+
+    [SerializeField]
+    private int spawnDelayInSeconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnGoblin());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnGoblin()
+    {
+        yield return new WaitForSeconds(spawnDelayAtStart);
+
+        while (true)
+        {
+            var goblin = Instantiate(goblinPrefab);
+            goblin.MoveDirection = goblinMoveDirection;
+
+            goblin.transform.position = transform.position;
+
+            yield return new WaitForSeconds(spawnDelayInSeconds);
+        }
+    }
+}
